e4d828f2087ff4013e8c6b36fecfb285f2217586
schla-ma <57615912+schla-ma@users.noreply.github.com>
2019-11-22 12:17:11 +0100

    test

--- src/main/java/com/wwi318/YourParty/Configuration/SecurityConfig.java
+++ src/main/java/com/wwi318/YourParty/Configuration/SecurityConfig.java
@@ -1,10 +1,14 @@
 package com.wwi318.YourParty.Configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -16,6 +20,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.anyRequest()
 			.permitAll()
 			.and().csrf().disable();
+	}
+	
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser("admin")
+			.password("admin")
+			.roles("Admin")
+			.and()
+			.withUser("yourparty")
+			.password("yourparty")
+			.roles("User");
+	}
+	
+	public PasswordEncoder getPasswordEncoder() {
+		return NoOpPasswordEncoder.getInstance() ;
+		
 	}
 
 }
