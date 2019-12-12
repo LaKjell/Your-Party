ef6fae45e59717d7eb040532807b9101b6c37f36
LaKjell <57615935+LaKjell@users.noreply.github.com>
2019-12-12 16:33:05 +0100

    Datenabfrage

--- src/main/resources/static/js/JS_Profil.js
+++ src/main/resources/static/js/JS_Profil.js
@@ -8,21 +8,21 @@ function My_Profil() {
 		
 		var boxes = document.getElementById('flex');
 		
-		var col3l = document.createElement('div');
+		var col4l = document.createElement('div');
 		var col12l = document.createElement('div');
-		var col6 = document.createElement('div');
+		var col8 = document.createElement('div');
 		var col12m = document.createElement('div');
 		var col3r = document.createElement('div');
 		var col12r = document.createElement('div');
 		
-		col3l.setAttribute('class','col-md-3 bord');
+		col4l.setAttribute('class','col-md-4 bord');
 		col12l.setAttribute('class','col-md-12 jumbotron');
-		col6.setAttribute('class','col-md-6 bord');
+		col8.setAttribute('class','col-md-8 bord');
 		col12m.setAttribute('class','col-md-12 jumbotron');
-		col3r.setAttribute('class','col-md-3 bord');
-		col12r.setAttribute('class','col-md-12 jumbotron');
+//		col3r.setAttribute('class','col-md-3 bord');
+//		col12r.setAttribute('class','col-md-12 jumbotron');
 		
-		col12l.innerHTML = '<img class="d-block w-100" alt="picture" src="/img/Saal.jpg">';
+		col12l.innerHTML = '<img class="d-block w-100" alt="picture" src="/downloadFile/' + user.username + '.jpg">';
 		
 		var h2m1 = document.createElement('h2');
 		var pm1 = document.createElement('p');
@@ -31,8 +31,8 @@ function My_Profil() {
 		/**
 		pm1.innerHTML= "Vorname: <br> &nbsp;&nbsp;&nbsp;&nbsp;	" + user.firstname + "<br> <br> Nachname: <br> &nbsp;&nbsp;&nbsp;&nbsp;" + user.lastname + "<br> <br> Geschlecht:<br> &nbsp;&nbsp;&nbsp;&nbsp; " + user.gender + "<br><br>  Geburtsdatum:<br> &nbsp;&nbsp;&nbsp;&nbsp; " + user.gg ;
 		*/
-		if(user.gender == "m"){pm1.innerHTML= "Vorname: <br> &nbsp;&nbsp;&nbsp;&nbsp;	" + user.firstname + "<br> <br> Nachname: <br> &nbsp;&nbsp;&nbsp;&nbsp;" + user.lastname + "<br> <br> Geschlecht:<br> &nbsp;&nbsp;&nbsp;&nbsp; " + "Männlich" + "<br><br>  Geburtsdatum:<br> &nbsp;&nbsp;&nbsp;&nbsp; " + user.birthdate ;}
-		else{pm1.innerHTML="Vorname: <br> &nbsp;&nbsp;&nbsp;&nbsp;	" + user.firstname + "<br> <br> Nachname: <br> &nbsp;&nbsp;&nbsp;&nbsp;" + user.lastname + "<br> <br> Geschlecht:<br> &nbsp;&nbsp;&nbsp;&nbsp; " + "Weiblich" + "<br><br>  Geburtsdatum:<br> &nbsp;&nbsp;&nbsp;&nbsp; " + birthdate;;};
+		if(user.gender == "m"){pm1.innerHTML= "Vorname:  &nbsp;&nbsp;&nbsp;&nbsp;	" + user.firstname + "<br> <br> Nachname:  &nbsp;&nbsp;&nbsp;&nbsp;" + user.lastname + "<br> <br> Geschlecht:&nbsp;&nbsp;&nbsp;&nbsp; " + "Männlich" + "<br><br>  Geburtsdatum: &nbsp;&nbsp;&nbsp;&nbsp; " + user.birthdate ;}
+		else{pm1.innerHTML="Vorname:  &nbsp;&nbsp;&nbsp;&nbsp;	" + user.firstname + "<br> <br> Nachname:  &nbsp;&nbsp;&nbsp;&nbsp;" + user.lastname + "<br> <br> Geschlecht:&nbsp;&nbsp;&nbsp;&nbsp; " + "Weiblich" + "<br><br>  Geburtsdatum: &nbsp;&nbsp;&nbsp;&nbsp; " + birthdate;;};
 		col12m.appendChild(h2m1);
 		col12m.appendChild(pm1);
 		
@@ -49,7 +49,7 @@ function My_Profil() {
 		var pm3 = document.createElement('p');
 		
 		h2m3.innerHTML= "Kontakt zu dir";
-		pm3.innerHTML= user.email + "&nbsp;&nbsp;&nbsp;&nbsp;" + user.phonenumber;
+		pm3.innerHTML= user.email + "&nbsp;&nbsp;&nbsp;&nbsp; <br>" + user.phonenumber;
 		
 		col12m.appendChild(h2m3);
 		col12m.appendChild(pm3);
@@ -63,12 +63,12 @@ function My_Profil() {
 		col12m.appendChild(h2m4);
 		col12m.appendChild(pm4);
 		
-		col3l.appendChild(col12l);
-		col6.appendChild(col12m);
-		col3r.appendChild(col12r);
+		col4l.appendChild(col12l);
+		col8.appendChild(col12m);
+//		col3r.appendChild(col12r);
 		
-		boxes.appendChild(col3l);
-		boxes.appendChild(col6);
-		boxes.appendChild(col3r);
+		boxes.appendChild(col4l);
+		boxes.appendChild(col8);
+//		boxes.appendChild(col3r);
 	});
 }
