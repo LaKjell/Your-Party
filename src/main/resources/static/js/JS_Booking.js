88567ead7cb10184751cef3a20830de266dae603
Preiss <Preiss@Preiss-PC.speedport.ip>
2019-12-12 16:58:22 +0100

    Änderung

--- src/main/resources/static/js/JS_Booking.js
+++ src/main/resources/static/js/JS_Booking.js
@@ -5,7 +5,6 @@ function Booking(){
 	
 var liste = storage.getAll();
 
-alert(liste);
 document.getElementById('locationId').setAttribute('value','liste.id');
 $.get("username", function(data){
 	var user = data;
